3cc836f020cf1ec729dda3e77363a47b5e8e85e1
boyallex <60341259+boyallex@users.noreply.github.com>
2021-08-24 16:11:47 +0300

    Update login_service.dart
    - дописал запрос в БД

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -1,1 +1,10 @@
+import 'package:test_1/modules/login_data.dart';
+
 import '../test_globals.dart' as globals;
+
+class LoginService {
+  Future<List<LoginData>> getData() async{
+    final data = globals.json;
+    return await Future.delayed(Duration()).then((_)=>data.map((e) => LoginData.fromJson(e)).toList());
+  }
+}
