0b5d6503decd9b1b6da07ed7f68658e446188079
boyallex <60341259+boyallex@users.noreply.github.com>
2021-08-24 15:52:21 +0300

    Service and DB creating
    -создал тестовую базу данных пользователей

---
+++ lib/test_globals.dart
@@ -0,0 +1,15 @@
+library test_1.globals;
+final json = [
+  {
+    "name": "Даниил",
+    "login": "qwerty@mail.ru",
+    "email":"qwerty@mail.ru",
+    "password": "password123",
+  },
+  {
+    "name": "Михаил",
+    "login": "asdf@mail.ru",
+    "email": "asdf@mail.ru",
+    "password": "qwerty1234",
+  }
+];
