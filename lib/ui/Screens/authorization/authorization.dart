b7f0bc8e511f3c766c3b6786aa6990a41c82bb47
boyallex <60341259+boyallex@users.noreply.github.com>
2021-08-27 16:07:25 +0300

    Dev
    - внедряю новый Bloc в UI

--- lib/ui/Screens/authorization/authorization.dart
+++ lib/ui/Screens/authorization/authorization.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_1/assets/strings.dart';
-import 'package:test_1/bloc/login_bloc/login_bloc.dart';
-import 'package:test_1/bloc/sign_up_bloc/sign_up_bloc.dart';
+import 'package:test_1/bloc/auth_bloc/auth_bloc.dart';
 import 'package:test_1/ui/Screens/Authorization/start_screen.dart';
 import 'package:test_1/ui/Screens/authorization/log_in_fields.dart';
 import 'package:test_1/ui/Screens/authorization/sign_up_fields.dart';
-import 'package:test_1/ui/Widgets/entering_field.dart';
 import 'package:test_1/ui/Widgets/warning_field.dart';
 
 class Authorization extends StatelessWidget {
@@ -22,33 +20,48 @@ class Authorization extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bloc = type == PageType.authorization ? SignUpBloc() : LoginBloc();
+    final bloc = AuthBloc();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(Strings.Auth),
-      ),
-      body: Column(
-        children: [
-          // верстка полей
-          getFields(this.type!),
-          // блок билдер, контролирующий поле ошибок
-          getBloc(this.type!, bloc),
-          ElevatedButton(
-            onPressed: () {
-              type == PageType.authorization
-                  ? this.onPressedRegistrateButton(bloc)
-                  : this.onPressedLoginButton(bloc);
-            },
-            child: SizedBox(
-              child: Center(
-                child: Text(Strings.sign_up),
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(Strings.Auth),
+        ),
+        body: Column(
+          children: [
+            // верстка полей
+            getFields(this.type!),
+            // блок билдер, контролирующий поле ошибок
+            // getBloc(this.type!, bloc),
+            BlocBuilder<AuthBloc, AuthState>(
+                bloc: bloc,
+                builder: (context, state) {
+                  if (state is SignUpFailed) {
+                    warning = state.warning;
+                    return WarningField(isVisible: true, text: warning);
+                  } else {
+                    return Container();
+                  }
+                }),
+            ElevatedButton(
+              onPressed: () {
+                type == PageType.authorization
+                    ? this.onPressedRegistrateButton(bloc)
+                    : this.onPressedLoginButton(bloc);
+              },
+              child: SizedBox(
+                child: Center(
+                  child: Text(Strings.sign_up),
+                ),
+                height: 40,
+                width: 150,
               ),
-              height: 40,
-              width: 150,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -73,34 +86,34 @@ class Authorization extends StatelessWidget {
     );
   }
 
-  BlocBuilder getBloc(PageType authType, dynamic bloc) {
-    if (authType == PageType.authorization) {
-      return BlocBuilder<SignUpBloc, SignUpState>(
-          bloc: bloc,
-          builder: (context, state) {
-            if (state is SignUpFailed) {
-              warning = state.warning;
-              return WarningField(isVisible: true, text: warning);
-            } else {
-              return Container();
-            }
-          });
-    } else {
-      return BlocBuilder<LoginBloc, LoginState>(
-        bloc: bloc,
-        builder: (context, state) {
-          if (state is LoginFailed) {
-            return WarningField(
-              isVisible: true,
-              text: state.warning,
-            );
-          } else {
-            return Container();
-          }
-        },
-      );
-    }
-  }
+  // BlocBuilder getBloc(PageType authType, dynamic bloc) {
+  //   if (authType == PageType.authorization) {
+  //     return BlocBuilder<AuthBloc, AuthState>(
+  //         bloc: bloc,
+  //         builder: (context, state) {
+  //           if (state is SignUpFailed) {
+  //             warning = state.warning;
+  //             return WarningField(isVisible: true, text: warning);
+  //           } else {
+  //             return Container();
+  //           }
+  //         });
+  //   } else {
+  //     return BlocBuilder<Bloc, LoginState>(
+  //       bloc: bloc,
+  //       builder: (context, state) {
+  //         if (state is LoginFailed) {
+  //           return WarningField(
+  //             isVisible: true,
+  //             text: state.warning,
+  //           );
+  //         } else {
+  //           return Container();
+  //         }
+  //       },
+  //     );
+  //   }
+  // }
 
   Widget getFields(PageType authType) {
     var fields;
@@ -118,3 +131,5 @@ class Authorization extends StatelessWidget {
     return fields;
   }
 }
+
+class SubjectState {}
