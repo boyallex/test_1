79194694919fa61a9c5882facf7085b1f3091015
boyallex <60341259+boyallex@users.noreply.github.com>
2021-09-18 16:46:23 +0300

    Profile Make up
    - верстка главного экрана

--- lib/ui/Screens/main/main_screen.dart
+++ lib/ui/Screens/main/main_screen.dart
@@ -13,10 +13,54 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        child: Center(
-          child: Image(
-            image: AssetImage("images/pic.jpg"),
-          ),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Image(
+                image: AssetImage('images/suicide.jpg'),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.white),
+                shadowColor: MaterialStateProperty.all(Colors.white),
+                
+              ),
+            ),
+            SizedBox(
+              child: Center(
+                child: Text("Вот эта жоска"),
+              ),
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Image(
+                image: AssetImage('images/give_up.jpg'),
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white)),
+            ),
+            SizedBox(
+              child: Center(
+                child: Text("Вот эта жоска"),
+              ),
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Image(
+                image: AssetImage('images/bleat.jpg'),
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white)),
+            ),
+            SizedBox(
+              child: Center(
+                child: Text("Вот эта жоска"),
+              ),
+              height: 30,
+            ),
+          ],
         ),
       ),
     );
